30571e7b2d8c6b70f8ba9442535e7dbd66b96930
Geliozzz <o.pocheptsov@gmail.com>
2014-11-27 12:39:54 +0300

    1

--- app/src/main/java/ru/oxbao/sensor_test/SensorInput.java
+++ app/src/main/java/ru/oxbao/sensor_test/SensorInput.java
@@ -6,7 +6,6 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 
 public class SensorInput implements SensorEventListener {
-<<<<<<< HEAD
     private Collector m_collectorOwner;
     private TestExecutorActivity m_ownerActivity;
 
@@ -17,11 +16,6 @@ public class SensorInput implements SensorEventListener {
 
     public void Start(){
 
-=======
-    Collector m_collector;
-    public void Start(Collector collector){
-         m_collector = collector;
->>>>>>> 66531d8cde43049e596bd73b4650f29978cb631b
     }
 
     public void Stop(){
@@ -30,13 +24,9 @@ public class SensorInput implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-<<<<<<< HEAD
            //  В датчике внутренний датчик реализован (он сам генерирует события выдачи новых данных)
             //В любом случае xxxInput циклично дёргает метод Collector’a
             m_collectorOwner.Amass();
-=======
-           m_collector.Collect();
->>>>>>> 66531d8cde43049e596bd73b4650f29978cb631b
     }
 
     @Override
